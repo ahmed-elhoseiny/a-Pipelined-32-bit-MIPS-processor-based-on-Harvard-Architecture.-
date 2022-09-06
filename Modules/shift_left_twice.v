ea18f20c034a9eca490a5f25fd300114124e0026
Ahmed Mahmoud Hoseiny <ahmedelhosseini53@gmail.com>
2022-09-06 17:41:21 +0200

    Update shift_left_twice.v

--- Modules/shift_left_twice.v
+++ Modules/shift_left_twice.v
@@ -1,6 +1,9 @@
-module shift_left_twice #(parameter WIDTH =32) (
-input        wire       [WIDTH-1:0]      IN,
-output       wire       [WIDTH-1:0]      OUT
+module shift_left_twice #(
+parameter IN_width  =32,
+          OUT_width =32
+) (
+input        wire       [IN_width-1:0]      IN,
+output       wire       [OUT_width-1:0]     OUT
 );
 
 assign OUT = (IN << 2);
