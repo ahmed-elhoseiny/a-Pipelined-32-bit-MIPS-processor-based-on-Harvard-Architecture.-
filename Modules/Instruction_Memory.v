fee177cf08db91e263021a3ef29f6bc6b0d8dff7
Ahmed Mahmoud Hoseiny <ahmedelhosseini53@gmail.com>
2022-09-06 06:50:26 +0200

    Update Instruction_Memory.v

--- Modules/Instruction_Memory.v
+++ Modules/Instruction_Memory.v
@@ -2,8 +2,8 @@ module Instruction_Memory #(
 parameter WIDTH = 32 , 
           DEPTH = 100
 ) (
-input  wire     [WIDTH-1:0]		    A,
-output wire      [WIDTH-1:0] 	    RD
+input  wire      [WIDTH-1:0]		 A,
+output wire      [WIDTH-1:0] 	     RD
 );
 
 reg [WIDTH-1:0] ROM [0:DEPTH-1];
