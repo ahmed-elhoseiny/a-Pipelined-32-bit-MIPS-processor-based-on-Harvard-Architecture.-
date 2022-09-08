c91dde65ff96a57922889ae6a397b8d0c8cb2197
Ahmed Mahmoud Hoseiny <ahmedelhosseini53@gmail.com>
2022-09-08 09:28:19 +0200

    Update Hazard_Unit.v

--- Modules/Hazard_Unit.v
+++ Modules/Hazard_Unit.v
@@ -1,7 +1,32 @@
 module Hazard_Unit #(
-parameter =,
+parameter Rs_width = 5,
+          Rt_width = 5,
+          WriteReg = 5
 ) (
 
+input       wire                    BranchD,
+input       wire                    JumpD,
+input       wire                    RsD,
+input       wire                    RtD,
+input       wire                    RsE,
+input       wire                    RtE,
+input       wire                    WriteRegE,
+input       wire                    RegWriteE,
+input       wire                    RegWriteM,
+input       wire                    WriteRegM,
+input       wire                    RegWriteW,
+input       wire                    WriteRegW,
+input       wire                    MemtoRegE,
+
+output      reg                     ForwardAE,
+output      reg                     ForwardBE,
+output      reg                     FlushE,
+output      reg                     ForwardAD,
+output      reg                     ForwardBD,
+output      reg                     StallF,
+output      reg                     StallD,
 );
-    
+
+
+
 endmodule
