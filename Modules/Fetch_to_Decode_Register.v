1cc3aeb2c6fdc05689450c02f2144f073628ad85
Ahmed Mahmoud Hoseiny <ahmedelhosseini53@gmail.com>
2022-09-10 23:08:03 +0200

    Update Fetch_to_Decode_Register.v

--- Modules/Fetch_to_Decode_Register.v
+++ Modules/Fetch_to_Decode_Register.v
@@ -2,12 +2,15 @@ module Fetch_to_Decode_Register #(
 parameter Instr_width = 32;
           PCPlus4_width = 32;
 ) (
+input wire                                CLK;
+input wire                                RST;
+input wire                                CLR;
+input wire                                EN;
 input wire      [Instr_width-1:0]         InstrF;
 input wire      [PCPlus4_width-1:0]       PCPlus4F;
 
 output reg      [Instr_width-1:0]         InstrD;
 output reg      [PCPlus4_width-1:0]       PCPlus4D;
-
 );
     
 endmodule
