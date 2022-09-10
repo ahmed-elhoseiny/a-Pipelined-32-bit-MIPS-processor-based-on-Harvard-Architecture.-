969a77e4cade59c2e9b22a85aae2fdbd28f07661
Ahmed Mahmoud Hoseiny <ahmedelhosseini53@gmail.com>
2022-09-10 23:29:00 +0200

    Update Fetch_to_Decode_Register.v

--- Modules/Fetch_to_Decode_Register.v
+++ Modules/Fetch_to_Decode_Register.v
@@ -12,5 +12,18 @@ input wire      [PCPlus4_width-1:0]       PCPlus4F;
 output reg      [Instr_width-1:0]         InstrD;
 output reg      [PCPlus4_width-1:0]       PCPlus4D;
 );
-    
+
+always @(posedge CLK or negedge RST) begin
+   if (!RST) begin
+    InstrD <= 'b0;
+    PCPlus4D <= 'b0;
+   end else if (!EN && !CLR) begin
+    InstrD <= InstrF;
+    PCPlus4D <= PCPlus4F;
+   end else if (!EN && CLR) begin
+    InstrD <= 'b0;
+    PCPlus4D <= 'b0;
+   end
+end
+
 endmodule
