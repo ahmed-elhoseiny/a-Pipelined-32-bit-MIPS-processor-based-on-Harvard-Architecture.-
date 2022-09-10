9de87420c3d9f3f977f0aceb60a42cd912263e7c
Ahmed Mahmoud Hoseiny <ahmedelhosseini53@gmail.com>
2022-09-11 01:48:36 +0200

    Update Execute_to_Memory_Register.v

--- Modules/Execute_to_Memory_Register.v
+++ Modules/Execute_to_Memory_Register.v
@@ -19,5 +19,20 @@ output reg      [ALUOut_width-1:0]           ALUOutM,
 output reg      [WriteData_width-1:0]        WriteDataM,
 output reg      [WriteReg_width-1:0]         WriteRegM
 );
-    
+
+reg [3+ALUOut_width+WriteData_width+WriteReg_width-1:0]    OUT_s;
+reg [3+ALUOut_width+WriteData_width+WriteReg_width-1:0]    IN_s;
+
+assign  {RegWriteM, MemtoRegM, MemWriteM, ALUOutM, WriteDataM, WriteRegM} = OUT_s ;
+assign  IN_s = {RegWriteE, MemtoRegE, MemWriteE, ALUOutE, WriteDataE, WriteRegE} ;
+
+always @(posedge CLK or negedge RST) begin
+   if (!RST) begin
+    OUT_s <= 'd0;
+   end else begin
+    OUT_s <= IN_s;
+   end
+end
+
+
 endmodule
