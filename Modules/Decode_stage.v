e2f8f7dfbee2a34e3e7a74f65877f3f65d7d0a9b
Ahmed Mahmoud Hoseiny <ahmedelhosseini53@gmail.com>
2022-09-13 11:15:49 +0200

    Update Decode_stage.v

--- Modules/Decode_stage.v
+++ Modules/Decode_stage.v
@@ -46,6 +46,95 @@ wire     [ALUOut_width-1:0]         MUX_D_RD1_or_ALUOutM;
 wire     [ALUOut_width-1:0]         MUX_D_RD2_or_ALUOutM;
 
 
+Control_Unit #(
+            .Funct_width(),
+            .Opcode_width(),  
+            .ALU_Control_width(),
+            .ALU_OP_width ()
+            ) Control_Unit_U1
+(
+.Opcode(),
+.Funct(),
 
-    
+.RegWrite(),
+.MemtoReg(),
+.MemWrite(),
+.ALUControl(),
+.ALUSrc(),
+.RegDst(),
+.Jump(),
+.Branch()
+);
+
+AND_gate #(.WIDTH() ) AND_gate_BranchD_EqualD_U1
+(
+.In1(),
+.In2(),
+
+.out()
+);
+
+
+OR_gate #(.WIDTH() ) OR_gate_PCSrcD_0_1_U1
+(
+.In1(),
+.In2(),
+
+.out()
+);
+
+comparator #(
+           .in1_width(),   
+           .in2_width()
+            ) comparator_EqualD_U1
+(
+.in1(),
+.in2(),
+
+.out_signal()
+);
+
+MUX_2_to_1 #(.WIDTH()) MUX_2_to_1_RD1_U1
+(
+.In1(),
+.In2(),
+.sel(),
+
+.out()
+);
+
+MUX_2_to_1 #(.WIDTH()) MUX_2_to_1_RD2_U2
+(
+.In1(),
+.In2(),
+.sel(),
+
+.out()
+);
+
+
+Register_File #(
+                .WIDTH(),
+                .DEPTH(), 
+                .REG_ADDR()
+               )  Register_File_U1
+( 
+.A1(),
+.A2(),
+.A3(),
+.WE3(),
+.WD3(),
+.CLK(),
+.RST(),
+
+.RD1(),
+.RD2()
+);
+
+
+
+
+
+
+
 endmodule
