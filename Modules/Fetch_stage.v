d48753841a46390cf6b6c596c407c91d62889f6b
Ahmed Mahmoud Hoseiny <ahmedelhosseini53@gmail.com>
2022-09-13 02:05:16 +0200

    Update Fetch_stage.v

--- Modules/Fetch_stage.v
+++ Modules/Fetch_stage.v
@@ -21,18 +21,48 @@ output wire     [Instr_width-1:0]          InstrF
 wire [PCF_width-1:0]    PCF;
 wire [PC_in_width-1:0]  PC_in;
 
-MUX_4_to_1  #(  .IN_WIDTH(), 
-                .OUT_WIDTH(),
-                .sel_WIDTH()
-                )   PC_in_MUX_4_to_1_U1 
+MUX_4_to_1  #(  .IN_WIDTH(PCPlus4_width), 
+                .OUT_WIDTH(PC_in_width),
+                .sel_WIDTH(PCSrcD_width)
+             )   MUX_4_to_1_PC_in_U1 
 (
-.In1(),
-.In2(),
-.In3(),
-.In4(),
-.sel(),
-.out()
+.In1(PCPlus4F),
+.In2(PCBranchD),
+.In3(PCJump),
+.In4('d0),
+.sel(PCSrcD),
+.out(PC_in)
 );
+
+
+Program_Counter #( .PC_in_width(PC_in_width),
+                   .PC_out_width(PCF_width)
+                 ) Program_Counter_PCF_U1
+(
+.CLK(CLK),
+.RST(RST),
+.EN(StallF),
+.PC_in(PC_in),
+.PC_out(PCF)
+);
+
+
+Adder #( .WIDTH(PCF_width) )  Adder_PCPlus4F_U1
+(
+.IN_1(PCF),
+.IN_2('d4),
+.OUT(PCPlus4F)
+);
+
+
+Instruction_Memory #( .WIDTH(PCF_width), 
+                      .DEPTH(100)
+                    )   Instruction_Memory_InstrF_U1
+(
+.A(PCF),
+.RD(InstrF)
+);
+
 
 
 
